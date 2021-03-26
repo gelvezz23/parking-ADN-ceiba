c3cfea3fe1673bc1bbc2ddd7871062c94e60a77f
Carlos Mario Gómez Gelvez <carlos.gelvez@ceiba.com.co>
2021-03-26 16:21:25 -0500

    update ofr test

--- src/app/shared/utils/CalculatePrice/calculatePaymentWithTime.ts
+++ src/app/shared/utils/CalculatePrice/calculatePaymentWithTime.ts
@@ -1,16 +1,17 @@
 export const calculatePaymentWithTime = (time: string, price: number) => {
   const formatData = time.split(':');
   let priceByHour = 0;
-  const days = parseInt(formatData[0]) * 24;
-  const hours = parseInt(formatData[1]);
-  const minutes = parseInt(formatData[2]);
+  const dayInhours = 24;
+  const days = parseInt(formatData[0], 10) * dayInhours;
+  const hours = parseInt(formatData[1], 10);
+  const minutes = parseInt(formatData[2], 10);
 
   const lessMinutes = 59;
   const priceLessHours = 700;
 
   if (minutes <= lessMinutes && days === 0 && hours === 0) {
-    return (priceByHour = priceLessHours);
+    return (priceByHour += priceLessHours);
   } else {
-    return (priceByHour = (days + hours) * price);
+    return (priceByHour += (days + hours) * price);
   }
 };
